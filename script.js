6b0b357ddaaf75f4b417647daca481cbf8a5c707
KCH001 <42988476+KCH001@users.noreply.github.com>
2019-08-06 07:58:13 +1200

    Add script.js via upload
    Java is still not working

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-function imgSwitch(x) {
-    if (x.matches) {
+function imgSwitch(z) {
+    if (z.matches) {
         document.getElementById("myImage").src = '3._Images/EM-LOGO3.png';
     } else {
         document.getElementById("myImage").src = '3._Images/EM-LOGO-BANNER-T.png';
     }
 }
 
-var x = window.matchMedia("(max-width: 650px)")
-imgSwitch(x) /*// Call listener function at run time*/
-x.addListener(imgSwitch) /*// Attach listener function on state changes*/
+var z = window.matchMedia("(max-width: 650px)")
+imgSwitch(z) /*// Call listener function at run time*/
+z.addListener(imgSwitch) /*// Attach listener function on state changes*/
 
 function Topnav() {
     var t = document.getElementById("myTopnav");
